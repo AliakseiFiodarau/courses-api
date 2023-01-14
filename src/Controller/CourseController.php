128723f239baccc48b05a9b88220cc7390c8d0c3
Aliaksei <ramone009@gmail.com>
2023-01-14 22:16:59 +0300

    Updated CourseController.php. Switched off form csrf validation.

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Course;
-use Doctrine\ORM\EntityManagerInterface;
+use App\Form\CourseFormType;
+use App\Repository\CourseRepository;
+use DateTimeImmutable;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,43 +14,131 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 class CourseController extends AbstractController
 {
     public function __construct(
-        private readonly EntityManagerInterface $em,
+        private readonly CourseRepository $repository
     ) {}
 
     #[Route(
         '/course/{id}',
-        methods: ['GET'],
-        name: 'course_show
-            ')]
+        name: 'course_show',
+        methods: ['GET']
+    )]
     public function show(int $id): JsonResponse
     {
-        $repository = $this->em->getRepository(Course::class);
-        $course = $repository->find($id);
+        $course = $this->repository->find($id);
 
-        if (!$course) {
+        if ($course) {
             return $this->json([
-                'error' => 'No entity found for id ' . $id
+                'course' => $course,
             ]);
         }
 
         return $this->json([
-            'course' => $course,
+            'message' => 'No entity found for id ' . $id
         ]);
     }
 
-
     #[Route(
         '/course',
-        methods: ['GET'],
-        name: 'course'
+        name: 'course',
+        methods: ['GET']
     )]
     public function index(): JsonResponse
     {
-        $repository = $this->em->getRepository(Course::class);
-        $courses = $repository->findAll();
+        $courses = $this->repository->findAll();
 
         return $this->json([
             'courses' => $courses,
+        ]);
+    }
+
+    #[Route(
+        '/course/create',
+        name: 'course_create',
+        methods: ['POST']
+    )]
+    public function create(Request $request): JsonResponse
+    {
+        $course = new Course;
+
+        $body = $request->getContent();
+        $data = json_decode($body, true);
+
+        $form = $this->createForm(CourseFormType::class, $course);
+        $form->submit($data);
+
+        if ($form->isValid()) {
+            $courseName = $form->getData()->getName();
+            $course->setName($courseName);
+            $course->setCreatedAt(new DateTimeImmutable('now'));
+
+            $this->repository->save($course, true);
+
+            $courseId = $course->getId();
+
+            return $this->json([
+                'message' => 'Course resource with id ' . $courseId . ' created'
+            ]);
+        }
+
+        $errors = $form->getErrors()->__toString();
+
+        return $this->json([
+            'message' => 'Unable to create resource. ' . $errors
+        ]);
+    }
+
+    #[Route(
+        '/course/update/{id}',
+        name: 'course_update',
+        methods: ['POST']
+    )]
+    public function update(Request $request, int $id): JsonResponse
+    {
+        $course = $this->repository->find($id);
+
+        $body = $request->getContent();
+        $data = json_decode($body, true);
+
+        $form = $this->createForm(CourseFormType::class, $course);
+        $form->submit($data);
+
+        if ($form->isValid()) {
+            $courseName = $form->getData()->getName();
+            $course->setName($courseName);
+
+            $this->repository->save($course, true);
+
+            return $this->json([
+                'message' => 'Resource with id ' . $id . ' has been updated'
+            ]);
+        }
+
+        $errors = $form->getErrors()->__toString();
+
+        return $this->json([
+            'message' => 'Unable to update resource. ' . $errors
+        ]);
+    }
+
+    #[Route(
+        '/course/delete/{id}',
+        name: 'course_delete',
+        methods: ['DELETE']
+    )]
+    public function delete(int $id): JsonResponse
+    {
+        $course = $this->repository->find($id);
+
+        if ($course) {
+            $this->repository->remove($course, true);
+
+            return $this->json([
+                'message' => 'Resource with id ' . $id . ' has been deleted'
+            ]);
+        }
+
+        return $this->json([
+            'message' => 'No entity found for id ' . $id
         ]);
     }
 }
